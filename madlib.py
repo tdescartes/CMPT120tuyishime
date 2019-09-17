18afa3886272d2501f0e497aeb788f1c25a2d99e
Your Name <descartestuyishime@148-100-152-109.FoxNet.marist.edu>
2019-09-17 18:12:25 -0400

    Solution to Mad Libs exercise.

---
+++ madlib.py
@@ -0,0 +1,13 @@
+#The program for string
+
+def main():
+
+    noun =input ("Enter a noun:")
+    verb =input ("Enter a verb:")
+    adjective =input ("Enter an Adjective: ")
+    place =input ("Enter a place:")
+    
+
+    print ("Take your ",adjective," ", noun," and ", verb, "it at the", place, "!")
+
+main()
