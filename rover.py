8727635ad57d9f590bfa03181cad7c0f0997f956
Descartes Tuyishime <descartestuyishime@148-100-152-109.FoxNet.marist.edu>
2019-09-17 17:30:27 -0400

    Solution to Curiosity rover exercise.

---
+++ rover.py
@@ -0,0 +1,8 @@
+#The program that calculate time taken to transmit pictures
+
+def main():
+    print("Time a photo takes from Curiosity to NASA is ", (34*pow(10, 6))/ 186000, "Seconds")
+
+
+main()
+    
