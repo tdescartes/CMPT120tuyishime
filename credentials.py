5b47658154813807a789b8d582f99d12ff360ec7
Your Name <descartestuyishime@148-100-152-109.FoxNet.marist.edu>
2019-10-01 18:16:49 -0400

    The first version of the program

---
+++ credentials.py
@@ -0,0 +1,36 @@
+#   CMPT 120 Intro to Programming
+#   Lab #5- Working with strings and Functions
+#   Created: 2019-10-01
+
+
+
+
+
+
+
+
+def main():
+
+    # Getting user's first and last name
+
+    first= input ("Enter your first name: ")
+    last= input ("Enter your first name: ")
+
+    # Generating a Marist-style username
+    
+    uname= first+ (".")+ last
+
+    # Getting and checking user's password
+
+    passwd= input ("Create a new password: ")
+
+    while (len(passwd)<8):
+        print ("Fool of a Took! That password is feeble!")
+        passwd= input ("Create a new password: ")
+    else:
+        print ("The force is strong in this one")
+        print("Account configured. Your new email address is", uname + "@marist.edu")
+    
+
+
+main()
