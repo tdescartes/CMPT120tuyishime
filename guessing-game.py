5c676e074e6248cf89af7cf446c9cd7e41bd4091
Your Name <descartestuyishime@148-100-152-109.FoxNet.marist.edu>
2019-10-15 18:41:04 -0400

    Usability improvements to the guessing game, resolves #M and #N.

--- guessing-game.py
+++ guessing-game.py
@@ -7,20 +7,24 @@ def main():
     
     myanswer= "Lion"
 
-    guess= input ("Think of an animal, guess what it is: ")
+    guess= (input ("Think of an animal, guess what it is: ")).lower()
 
     while True:
 
-        if myanswer.lower()!=guess.lower():
+        if guess[0]=="q":
+
+            break
+
+        elif myanswer.lower()!=guess:
 
             print ("Unfortunately, you missed. Try again Please")
             
             guess= input ("I am Thinking of an animal, guess what it is: ")
 
         else:
+            print ("Congratulation!! You finally got it correct!")
+            
             break
 
-    print ("Congratulation!! You finally got it correct!")
-
-
+    
 main()
