065131232e7cad634a0c0cece172a874ba93768f
Juan Arias <juan.arias@marist.edu>
2019-10-20 14:35:04 -0400

    Feedback

--- Project/calc_functions.py
+++ Project/calc_functions.py
@@ -2,12 +2,15 @@
 # Project #1 – Calculator Vo1
 # Created: 2019-10-15
 
+# JA: Move the main() function to calculate.py
 def main():
 
     string1= input ("Enter you calculation: ")
     mylist= string1.split(" ")
     length= len(mylist)
+    print(solve(mylist))
 
+def solve(mylist):
     # The look to search and use operator
     while length > 1:
         if "/" in mylist:
@@ -40,7 +43,7 @@ def main():
             
         length= len(mylist)
 
-    print ("The results is", mylist[0] )
+#    print ("The results is", mylist[0] )
 
 
 main()
